49b0956aad7ba07bf49a934db231829cce7da62d
jingay <boublegreen4@gmail.com>
2018-01-18 15:47:45 -0400

    added a dust.js

---
+++ dust.js
@@ -0,0 +1,7 @@
+ function money(){
+	    document.getElementById('num1').innerHTML = Math.floor(Math.random() * 20) + 1;
+		document.getElementById('num2').innerHTML = Math.floor(Math.random() * 20) + 1;
+		document.getElementById('num3').innerHTML = Math.floor(Math.random() * 20) + 1;
+		document.getElementById('num4').innerHTML = Math.floor(Math.random() * 20) + 1;
+		document.getElementById('num5').innerHTML = Math.floor(Math.random() * 20) + 1;
+	  }
